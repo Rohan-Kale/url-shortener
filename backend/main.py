690cf3f4bede3923bf255ab335c2ca7032500279
Rohan Kale <rohan@Rohans-MacBook-Pro.local>
2025-09-17 15:06:51 -0400

    fix creation of app

--- backend/main.py
+++ backend/main.py
@@ -1,2 +1,24 @@
-print("Hello, URL Shortener")
-    
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from typing import List
+
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# in memory database (does not run when app shuts down)
+memory_db = {}
+
